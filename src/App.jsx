97b13aa9d94cf3f92c8a64846fa23636aa25f995
Prophetia-Melitensis <thermal-multi.0m@icloud.com>
2024-03-22 16:46:48 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <div className="header">
+          <img src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' alt='github logo' className="github-logo"/>
+          <h1 className="heading">Prophetia Melitensis</h1>
+          <p className="desc">Prophecies of Malta</p>
+        </div>
+
+        
+      </div>
+    </>
+  )
+}
+
+export default App
