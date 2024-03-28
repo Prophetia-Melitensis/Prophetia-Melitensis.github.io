3d824765e92fb8b982c4794d1254285f95ca5e57
Prophetia-Melitensis <164334706+Prophetia-Melitensis@users.noreply.github.com>
2024-03-28 20:53:47 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.css'
 
 function App() {
 
+  const openImage = () => {
+    window.open("https://github.com/Prophetia-Melitensis", "_blank");
+  }
+
   return (
     <>
       <div>
         <div className="header">
-          <img src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' alt='github logo' className="github-logo"/>
+          <img src='https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png' alt='github logo' className="github-logo" onClick={openImage}/>
           <h1 className="heading">Prophetia Melitensis</h1>
           <p className="desc">Prophecies of Malta</p>
         </div>
